1b269d709bd2752d608629f5257ad9e3e7bc380e
alexandra <alexandra.deh643@gmail.com>
2022-03-25 20:40:53 -0300

    alexandra Gonçalves

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string fraseOriginal;
+
+Console.Write("Por favor, digite algo que tenha a letra R: ");
+fraseOriginal = Console.ReadLine()!;
+
+Console.WriteLine(fraseOriginal.Replace("r", "l").Replace("R", "L"));
